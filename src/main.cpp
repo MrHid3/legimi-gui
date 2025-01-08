3effae1212cc582780acd1a8c222fed0ac0d2c0d
MrHid3 <gkornas@proton.me>
2025-01-08 09:09:50 +0100

    napisałem dzisiaj conajmneij 7 razy więcej niz przez ostatnie pół roku (dalej zero funkcjonalności)

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,76 @@
+#include <any>
+#include <vector>
+
 #include "string"
 #include "cstdint"
+
+using readOp = int8_t;
+
+class Account {
+    std::string GetLogin();
+    std::string GetPassword();
+    std::string GetKindleId();
+    void SaveLogin();
+    void SavePassword();
+    void SaveKindleId();
+};
+
+class Client {
+
+};
+
+class IOWriter {
+    int Write(std::byte[]);
+};
+
+class IOReader {
+    int Read(std::byte[]);
+};
+
+class Encoder {
+    void Encode(IOWriter);
+    int EncodedLength();
+};
+
+class Decoder {
+    int Decoode(IOReader);
+};
+
+class Typed {
+    uint16_t Type();
+};
+
+class Request : Typed, Encoder{
+
+};
+
+class Response : Typed, Decoder{
+
+};
+
+class Formater {
+    std::string DebugFormat();
+};
+
+struct Buffer {
+    std::vector<std::byte> buf;
+    int off;
+    readOp lastRead;
+};
+
+void Exchange(Request request, Response response) {
+    Buffer* buf = new Buffer();
+
+};
+
+struct DefaultClient {
+    bool debugOn;
+    void debug(std::any value) {
+        if (this->debugOn) {
+
+        }
+    }
+};
 
 struct loginData {
     std::string login;
@@ -11,10 +82,13 @@ struct GetSession {
     uint64_t kindleID;
 };
 
-// encoder (encode.go)
-// o co chodzi z io.Writer?
-// odpowiednik encodowania w cpp
+struct defaultAccountService {
+    Account accountRepository;
 
+};
+
+// encoder (encode.go)
+// odpowiednik encodowania w cpp
 
 
 int main() {
